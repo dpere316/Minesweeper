5ed9aa183916ab52b1b26e7cfbf3936e9cb3b5cb
Daniel Perz <dpere316@fiu.edu>
2020-09-28 17:04:30 -0400

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,251 @@
+﻿namespace MineSweeper
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.easy = new System.Windows.Forms.RadioButton();
+            this.medium = new System.Windows.Forms.RadioButton();
+            this.expert = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.UserBox = new System.Windows.Forms.TextBox();
+            this.custom = new System.Windows.Forms.RadioButton();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.easyToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.mediumToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.expertToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.closeAllGamesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(244, 144);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Play";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Play);
+            // 
+            // easy
+            // 
+            this.easy.AutoSize = true;
+            this.easy.Location = new System.Drawing.Point(128, 106);
+            this.easy.Name = "easy";
+            this.easy.Size = new System.Drawing.Size(48, 17);
+            this.easy.TabIndex = 1;
+            this.easy.TabStop = true;
+            this.easy.Text = "Easy";
+            this.easy.UseVisualStyleBackColor = true;
+            this.easy.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // medium
+            // 
+            this.medium.AutoSize = true;
+            this.medium.Checked = true;
+            this.medium.Location = new System.Drawing.Point(199, 106);
+            this.medium.Name = "medium";
+            this.medium.Size = new System.Drawing.Size(62, 17);
+            this.medium.TabIndex = 2;
+            this.medium.TabStop = true;
+            this.medium.Text = "Medium";
+            this.medium.UseVisualStyleBackColor = true;
+            this.medium.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // expert
+            // 
+            this.expert.AutoSize = true;
+            this.expert.Location = new System.Drawing.Point(280, 106);
+            this.expert.Name = "expert";
+            this.expert.Size = new System.Drawing.Size(55, 17);
+            this.expert.TabIndex = 3;
+            this.expert.TabStop = true;
+            this.expert.Text = "Expert";
+            this.expert.UseVisualStyleBackColor = true;
+            this.expert.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(163, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Current User:";
+            // 
+            // UserBox
+            // 
+            this.UserBox.Location = new System.Drawing.Point(259, 35);
+            this.UserBox.Name = "UserBox";
+            this.UserBox.Size = new System.Drawing.Size(100, 20);
+            this.UserBox.TabIndex = 6;
+            this.UserBox.Text = "Guest";
+            this.UserBox.TextChanged += new System.EventHandler(this.UserBox_TextChanged);
+            this.UserBox.Enter += new System.EventHandler(this.UserBox_TextChanged);
+            // 
+            // custom
+            // 
+            this.custom.AutoSize = true;
+            this.custom.Location = new System.Drawing.Point(357, 106);
+            this.custom.Name = "custom";
+            this.custom.Size = new System.Drawing.Size(60, 17);
+            this.custom.TabIndex = 7;
+            this.custom.TabStop = true;
+            this.custom.Text = "Custom";
+            this.custom.UseVisualStyleBackColor = true;
+            this.custom.CheckedChanged += new System.EventHandler(this.radioButton4_CheckedChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(134, 65);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(127, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Number of Games Open: ";
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(259, 62);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 9;
+            this.textBox1.Text = "0";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1,
+            this.closeAllGamesToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(497, 24);
+            this.menuStrip1.TabIndex = 10;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.easyToolStripMenuItem,
+            this.mediumToolStripMenuItem,
+            this.expertToolStripMenuItem});
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(111, 20);
+            this.toolStripMenuItem1.Text = "Play a New Game";
+            // 
+            // easyToolStripMenuItem
+            // 
+            this.easyToolStripMenuItem.Name = "easyToolStripMenuItem";
+            this.easyToolStripMenuItem.Size = new System.Drawing.Size(119, 22);
+            this.easyToolStripMenuItem.Text = "Easy";
+            this.easyToolStripMenuItem.Click += new System.EventHandler(this.easyToolStripMenuItem_Click);
+            // 
+            // mediumToolStripMenuItem
+            // 
+            this.mediumToolStripMenuItem.Name = "mediumToolStripMenuItem";
+            this.mediumToolStripMenuItem.Size = new System.Drawing.Size(119, 22);
+            this.mediumToolStripMenuItem.Text = "Medium";
+            this.mediumToolStripMenuItem.Click += new System.EventHandler(this.mediumToolStripMenuItem_Click);
+            // 
+            // expertToolStripMenuItem
+            // 
+            this.expertToolStripMenuItem.Name = "expertToolStripMenuItem";
+            this.expertToolStripMenuItem.Size = new System.Drawing.Size(119, 22);
+            this.expertToolStripMenuItem.Text = "Expert";
+            this.expertToolStripMenuItem.Click += new System.EventHandler(this.expertToolStripMenuItem_Click);
+            // 
+            // closeAllGamesToolStripMenuItem
+            // 
+            this.closeAllGamesToolStripMenuItem.Name = "closeAllGamesToolStripMenuItem";
+            this.closeAllGamesToolStripMenuItem.Size = new System.Drawing.Size(104, 20);
+            this.closeAllGamesToolStripMenuItem.Text = "Close All Games";
+            this.closeAllGamesToolStripMenuItem.Click += new System.EventHandler(this.closeAllGamesToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(38, 20);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(497, 179);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.custom);
+            this.Controls.Add(this.UserBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.expert);
+            this.Controls.Add(this.medium);
+            this.Controls.Add(this.easy);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "MainForm";
+            this.Text = "MineSweeper";
+            this.Click += new System.EventHandler(this.gameCount);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RadioButton easy;
+        private System.Windows.Forms.RadioButton medium;
+        private System.Windows.Forms.RadioButton expert;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox UserBox;
+        private System.Windows.Forms.RadioButton custom;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem easyToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem mediumToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem expertToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem closeAllGamesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MineSweeper
+{
+    public partial class MainForm : Form
+    {
+
+        string name;
+        public MainForm()
+        {
+            InitializeComponent();
+
+           
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton4_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void Play(object sender, EventArgs e)
+        {
+            int row=0, col=0,mines = 0;//row*col >=18, mines <= row*col/2
+            String text = "";
+            Form2 f = null;
+            
+            if (easy.Checked)
+            {
+                row = col = 9;
+                mines = 10;
+                text = "Easy";
+            }
+            else if (medium.Checked)
+            {
+                row = col = 16;
+                mines = 40;
+                text = "Medium";
+            }
+            else if (expert.Checked)
+            {
+                row = 30;
+                col = 16;
+                mines = 99;
+                text = "Expert";
+            }
+            else
+                return;
+            int size = Math.Min(30, 1000 / Math.Max(row, col));
+            f = new Form2(text, row, col, size,mines);
+            f.Show();
+           
+
+
+        }
+
+       
+
+       
+        private void UserBox_TextChanged(object sender, EventArgs e)
+        {
+            name = UserBox.Text;   
+        }
+
+        private void gameCount(object sender, EventArgs e)
+        {
+            int count = 0;
+
+            foreach (Form f in Application.OpenForms)
+            {
+                if (f is Form2)
+                {
+                    count++;
+                }
+                textBox1.Text = count.ToString();
+
+            }
+        }
+
+        private void easyToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            int row = 0, col = 0, mines = 0;//row*col >=18, mines <= row*col/2
+            String text = "";
+            Form2 f = null;
+
+            row = col = 9;
+            mines = 10;
+            text = "Easy";
+
+            int size = Math.Min(30, 1000 / Math.Max(row, col));
+            f = new Form2(text, row, col, size, mines);
+            f.Show();
+
+        }
+
+        private void mediumToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            int row = 0, col = 0, mines = 0;//row*col >=18, mines <= row*col/2
+            String text = "";
+            Form2 f = null;
+
+            row = col = 16;
+            mines = 40;
+            text = "Medium";
+
+            int size = Math.Min(30, 1000 / Math.Max(row, col));
+            f = new Form2(text, row, col, size, mines);
+            f.Show();
+
+        }
+
+        private void expertToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            int row = 0, col = 0, mines = 0;//row*col >=18, mines <= row*col/2
+            String text = "";
+            Form2 f = null;
+
+            row = 30;
+            col = 16;
+            mines = 99;
+            text = "Expert";
+
+
+            int size = Math.Min(30, 1000 / Math.Max(row, col));
+            f = new Form2(text, row, col, size, mines);
+            f.Show();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void closeAllGamesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            List<Form> forms = new List<Form>();
+
+            foreach (Form f in Application.OpenForms)
+            {
+                if (f is Form2)
+                {
+                    forms.Add(f);
+                }
+            }
+
+                foreach(Form f in forms)
+                {
+                    f.Close();
+                }
+                
+            
+        }
+    }
+}
